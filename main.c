bda616a84636fb68c7286001932560d4b2f84a58
Alan Martin <153191025+Alanadriel@users.noreply.github.com>
2024-09-30 18:46:53 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "C:\Unlam\2C2024TOPICOS\Librerias\LVectores.h"
+#include "C:\Unlam\2C2024TOPICOS\Librerias\Matrices.h"
+#include "..\Librerias\Genericos.h"
+#include "..\Librerias\Cadenas.h"
+
+int main()
+{
+  char grup[]="abcdghijkoqtuv";
+  char cad[]="“Nj qemh v ljs kraenkqbres; lj oqe qemh es oqevorme sgn ellhs --Istqt Asdmgj";
+    Desofuscar(cad,grup);
+    printf("%s",cad );
+    return 0;
+}
+
